05077a07fad874553ad2c605ff98b83a00e2a23f
SagarKumar <73648743+mrbrainless@users.noreply.github.com>
2024-12-18 11:09:15 +0530

    Update app.js

--- posts/app.js
+++ posts/app.js
@@ -1,2 +1,1 @@
-document.head.innerHTML += '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5082315880457032"
-     crossorigin="anonymous"></script>';
+
